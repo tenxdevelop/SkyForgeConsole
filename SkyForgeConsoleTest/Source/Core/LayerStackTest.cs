872844aa518b76efa0b9052bc08bfb7bf879afd3
Tenxdev <tenxdeveloper@bk.ru>
2025-01-15 22:31:29 +0500

    Added more test for layerStack

--- SkyForgeConsoleTest/Source/Core/LayerStackTest.cs
+++ SkyForgeConsoleTest/Source/Core/LayerStackTest.cs
@@ -4,6 +4,7 @@
 
 using System;
 using NUnit.Framework;
+using SkyForgeConsole;
 
 namespace SkyForgeConsoleTest
 {
@@ -59,6 +60,23 @@ namespace SkyForgeConsoleTest
         }
 
         [Test]
+        public void CheckErrorWhenPushOverlayIsNull()
+        {
+            var layerStack = new LayerStack();
+            
+            Assert.Throws<ArgumentException>(() => layerStack.PushOverlay(null), "We push overlay is null in layerStack");
+            
+        }
+
+        [Test]
+        public void CheckErrorWhenPopOverlayIsNull()
+        {
+            var layerStack = new LayerStack();
+
+            Assert.Throws<ArgumentException>(() => layerStack.PopOverlay(null), "We pop overlay is null in layerStack");
+        }
+        
+        [Test]
         public void CheckPopLayerByLayer()
         {
             var fakeLayer = new FakeLayer();
@@ -70,6 +88,35 @@ namespace SkyForgeConsoleTest
             Assert.IsEmpty(layerStack);
         }
 
+        [Test]
+        public void CheckPopOverlayByLayer()
+        {
+            var fakeLayer = new FakeLayer();
+            var layerStack = new LayerStack();
+            layerStack.PushOverlay(fakeLayer);
+            Assert.IsNotEmpty(layerStack);
+            layerStack.PopOverlay(fakeLayer);
+            Assert.IsEmpty(layerStack);
+        }
+
+        [Test]
+        public void CheckGetLayerById()
+        {
+            var firstFakeLayer = new FakeLayer("layer1");
+            var secondFakeLayer = new FakeLayer("layer2");
+            var thirdFakeLayer = new FakeLayer("layer3");
+
+            var layerStack = new LayerStack();
+            
+            layerStack.PushLayer(firstFakeLayer);
+            
+            layerStack.PushOverlay(secondFakeLayer);
+            
+            layerStack.PushLayer(thirdFakeLayer);
+            
+            Assert.That(layerStack.GetLayer(1), Is.EqualTo(thirdFakeLayer));
+        }
+        
         [Test]
         public void CheckPushOverlayLayer()
         {
@@ -110,6 +157,30 @@ namespace SkyForgeConsoleTest
             var result = layerStack.GetLayers();
             Assert.That(result[0], Is.EqualTo(secondFakeLayer));
             Assert.That(result[1], Is.EqualTo(firstFakeLayer));
+        }
+
+        [Test]
+        public void CheckAddedToLogInfoWhenCalledErrorPushLayerIsNull()
+        {
+            Log.Init();
+            var fakeLogger = new FakeLogger();
+            Log.CoreLogger.AddLogger(fakeLogger);
+            var layerStack = new LayerStack();
+            Assert.Throws<ArgumentException>(() => layerStack.PushLayer(null), "We push layer is null in layerStack");
+            fakeLogger.CheckLog("We push layer is null in layerStack", 1);
+            fakeLogger.CheckLogLevelLogging(LogLevel.Error);
+        }
+
+        [Test]
+        public void CheckAddedToLogInfoWhenCalledErrorPopLayerIsNull()
+        {
+            Log.Init();
+            var fakeLogger = new FakeLogger();
+            Log.CoreLogger.AddLogger(fakeLogger);
+            var layerStack = new LayerStack();
+            Assert.Throws<ArgumentException>(() => layerStack.PopLayer(null), "We pop layer is null in layerStack");
+            fakeLogger.CheckLog("We pop layer is null in layerStack", 1);
+            fakeLogger.CheckLogLevelLogging(LogLevel.Error);
         }
     }
 }
